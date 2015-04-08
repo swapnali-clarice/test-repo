dc52612c0a604ce5f7eb5c640f4e7a77df245edd
Swapnali Bhagat <swapnamala.bhagat@claricetechnologies.com>
2015-04-08 17:33:33 +0530

    Added files

---
+++ app/src/androidTest/java/com/testdemo/testdemo/EmailTest.java
@@ -0,0 +1,22 @@
+package com.testdemo.testdemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+
+
+public class EmailTest extends InstrumentationTestCase {
+
+
+
+    public void testEmail() throws Exception {
+        String to = "user@edx.org";
+        String subject = "edX Test Email";
+        String email = "hi, this is test email";
+        EmailUtil.sendEmail(getInstrumentation()
+                .getTargetContext(), to, subject, email);
+        Log.d("Tag","sending email...");
+    }
+}
---
+++ app/src/androidTest/java/com/testdemo/testdemo/UtilTests.java
@@ -0,0 +1,21 @@
+package com.testdemo.testdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+
+public class UtilTests extends InstrumentationTestCase {
+    
+    public void testBrowserOpenUrl() throws Exception {
+        String url = "https://courses.edx.org/register";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse(url));
+        getInstrumentation().getTargetContext().startActivity(intent);
+
+        Log.d("URLTag", "finished open URL in browser");
+    }
+
+}
